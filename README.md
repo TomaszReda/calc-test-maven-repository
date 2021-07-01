92ad89bb33da11c5548cb873a8887668460b629f
treda <tomekreda16@gmail.com>
2021-07-01 19:46:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Example library for tutorial on publishing to Maven Central with OSSRH
+
---
+++ src/main/java/Calculator.java
@@ -0,0 +1,23 @@
+
+/**
+ * Really silly class that provides the same functionality as {@link int}
+ * itself for simple arithmetic operationsXD
+ */
+public class Calculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int substract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+}
